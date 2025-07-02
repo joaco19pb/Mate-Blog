4f7dcdd5485c52bc4d0ec352ec6a7ab62597afb7
Joaquin Garcia <joaquingarciatore@gmail.com>
2025-07-01 21:01:50 -0300

    Implementando modificaciones en el router y correcciones de seguridad

--- app/router.php
+++ app/router.php
@@ -5,26 +5,25 @@ define('BASE_PATH', __DIR__ . '/../');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($uri === '/') {
-    require BASE_PATH . 'app/controllers/index.php';
-}else if ($uri === '/historia') {
-    require BASE_PATH . 'app/controllers/historia.php';
-}else if ($uri === '/contacto') {
-    if ($requestMethod === 'GET') {
-        require BASE_PATH . 'app/controllers/contacto.php';
-    } else if ($requestMethod === 'POST') {
-        require BASE_PATH . 'app/views/contacto-resp.php';
-    }
-}else if ($uri === '/articulos') {
-    require BASE_PATH . 'app/controllers/articulos.php'; 
-}else if ($uri === '/beneficios-del-mate') {
-    require BASE_PATH . 'app/controllers/articulos/beneficios-del-mate.php';
-}else if ($uri === '/como-preparar-un-mate') {
-    require BASE_PATH . 'app/controllers/articulos/como-preparar-un-mate.php';
-}else if ($uri === '/como-curar-un-mate') {
-    require BASE_PATH . 'app/controllers/articulos/como-curar-un-mate.php';
-}else {
-    abort(); 
+$PATHS = [
+    'GET' => [
+        '/' => 'app/controllers/index.php',
+        '/historia'=>'app/controllers/historia.php',
+        '/contacto' => 'app/controllers/contacto.php',
+        '/articulos' =>  'app/controllers/articulos.php',
+        '/beneficios-del-mate' =>  'app/controllers/articulos/beneficios-del-mate.php',
+        '/como-preparar-un-mate' =>  'app/controllers/articulos/como-preparar-un-mate.php',
+        '/como-curar-un-mate' =>  'app/controllers/articulos/como-curar-un-mate.php',
+    ],
+    'POST' => [
+        '/contacto' => 'app/views/contacto-resp.php'
+    ]
+];
+
+if (isset($PATHS[$requestMethod][$uri])) {
+    require BASE_PATH . $PATHS[$requestMethod][$uri];
+} else {
+    abort();
 }
 
 function abort($code = 404){
--- app/views/contacto-resp.php
+++ app/views/contacto-resp.php
@@ -2,23 +2,13 @@
     <main class="container">
       <h2>Mensaje enviado</h2>
       <ul>
-        <li>Nombre: Armando</li>
-        <li>Apellido: Paredes</li>
-        <li>Mail: aparedes@mail.com</li>
-        <li>Fecha de nacimiento: 18/12/1998</li>
+        <li>Nombre:<?php echo htmlspecialchars($_POST['nombre'] ?? ''); ?></li>
+        <li>Apellido:<?php echo htmlspecialchars($_POST['apellido'] ?? ''); ?></li>
+        <li>Mail:<?php echo htmlspecialchars($_POST['email'] ?? ''); ?></li>
+        <li>Fecha de nacimiento:<?php echo htmlspecialchars($_POST['fecha_nacimiento'] ?? ''); ?></li>
 
         <p class="contact-resp__mesage">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          illo incidunt sapiente voluptates eveniet eligendi consequuntur ut,
-          autem consequatur porro quidem cumque tenetur, non dignissimos nostrum
-          vero atque eaque aliquid? Ullam at omnis dolorem animi minima voluptas
-          architecto dolorum corrupti. Ut, adipisci quaerat. Nam cum quidem odit
-          consequuntur error voluptas id ab! Debitis amet dolores eligendi
-          repellendus illo nobis repudiandae? Possimus minima, exercitationem
-          quo at aperiam hic iusto repudiandae tempora voluptatibus, officiis
-          eos, aspernatur earum dicta labore delectus! Possimus deserunt,
-          temporibus esse aspernatur consequatur doloremque qui quas ipsum odio
-          distinctio.
+          <?php echo nl2br(htmlspecialchars($_POST['mensaje'] ?? '')); ?>
         </p>
       </ul>
     </main>
